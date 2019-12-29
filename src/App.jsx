80af695940f2e9d12c084362923af5988cb022b4
liviaab <liviaab7@gmail.com>
2019-12-28 22:20:33 -0300

    Refactoring app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import CardImage from './components/card_image/CardImage'
 import './App.css'
+import loadingImg from './images/loading.gif'
+import sadDogImg from './images/sad-dog.png'
 
 function App() {
-  const [fetchedImageURL, setFetchedImageURL] = useState('')
+  const [fetchedImageURL, setFetchedImageURL] = useState(loadingImg)
+
+  const fetchImage = async () => {
+      setFetchedImageURL(loadingImg)
+      setTimeout(async () => {
+        try {
+          let response = await fetch('https://dog.ceo/api/breeds/image/random')
+          response = await response.json()
+          const imgURL = response.message
+          setFetchedImageURL(imgURL)
+        } catch(error) {
+          console.log('Error fetching image - ', error)
+          setFetchedImageURL(sadDogImg)
+          alert("Sorry, we could not fetch image")
+        }
+      }, 500)
+  }
 
   useEffect(() => {
-    async function fetchImage() {
-      try {
-        const response = await fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json())
-        const imgURL = response.message
-
-        setFetchedImageURL(imgURL)
-      } catch(error) {
-        console.log('Error fetching image - ', error)
-      }
-    }
-
     fetchImage()
   }, [])
 
@@ -24,8 +31,9 @@ function App() {
     <div className="App">
       <CardImage
         imageURL={fetchedImageURL}
-        description="Esse seria um texto para descrever alguma coisa sobre a raça desse dog"
+        description="Click the button and wait 0.5s to see a new doggo <3"
       />
+      <button type="button" onClick={fetchImage}>Get new image!</button>
     </div>
   );
 }
